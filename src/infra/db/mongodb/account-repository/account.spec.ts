178b7822589e7936e6d427cea962648de25206d9
Gabriel Asakawa <gabriel.asakawa@gmail.com>
2020-06-08 22:20:29 -0300

    feat: ensure mongodb reconnect if client is down

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -11,7 +11,7 @@ describe('Mongo respository', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
