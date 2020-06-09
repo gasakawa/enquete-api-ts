42d35c0fee3c291ba22d959ba95ee4490fe4d42a
Gabriel Asakawa <gabriel.asakawa@gmail.com>
2020-06-08 23:07:10 -0300

    fix: adjust function signature

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -4,8 +4,9 @@ import { DBAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptograpy/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { LogControllerDecorator } from '../decorators/log'
+import { Controller } from '../../presentation/protocols'
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const bcryptAdapter = new BcryptAdapter(12)
   const accountMongoRepository = new AccountMongoRepository()
